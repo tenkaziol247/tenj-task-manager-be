aee05582a9d9e1056bf3a3f711290334baced4bf
tenkaziol247 <tenkaziol247@gmail.com>
2021-01-24 00:17:48 +0700

    mini

--- src/db/db.js
+++ src/db/db.js
@@ -7,6 +7,7 @@ mongoose.connect(process.env.MONGODB_URL, {
     useFindAndModify: true,
 });
 
+//check connect db
 const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'Connection error:'));
 db.once('open', () => {
