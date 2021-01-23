bae3245772b5882ef795a9c00a6784c2d8d4699b
tenkaziol247 <tenkaziol247@gmail.com>
2021-01-24 00:08:47 +0700

    add cors

--- src/app.js
+++ src/app.js
@@ -1,9 +1,13 @@
 const express = require('express');
+const cors = require('cors');
+
 require('./db/db');
 const userRouter = require('./routers/user.router');
 const taskRouter = require('./routers/task.router');
 
 const app = express();
+
+app.user(cors());
 
 app.use(express.json());
 
