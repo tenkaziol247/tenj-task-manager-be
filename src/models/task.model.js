9cbf44eee25920e72b3629cecd52c6e126aef056
tenkaziol247 <tenkaziol247@gmail.com>
2021-01-27 22:06:25 +0700

    fix date field

--- src/models/task.model.js
+++ src/models/task.model.js
@@ -17,10 +17,19 @@ const taskSchema = new mongoose.Schema(
         },
         grade: {
             type: String,
+            trim: true,
+            lowercase: true,
             default: 'normal',
         },
-        date: {
+        range: {
             type: String,
+            trim: true,
+            lowercase: true,
+            required: true,
+        },
+        date: {
+            startAt: Date,
+            endAt: Date,
         },
         owner: {
             type: mongoose.Schema.Types.ObjectId,
@@ -32,6 +41,51 @@ const taskSchema = new mongoose.Schema(
         timestamps: true,
     },
 );
+
+taskSchema.statics.generateRangeProperty = (date) => {
+    let range;
+    if (!date) {
+        range = 'unset';
+    } else {
+        if (!date.startAt) {
+            range = 'nostarttime';
+        } else if (!date.endAt) {
+            range = 'noendtime';
+        } else {
+            if (date.endAt <= date.startAt) {
+                return;
+            } else {
+                const startTime = new Date(date.startAt);
+                const endTime = new Date(date.endAt);
+                const startAt = {
+                    year: startTime.getFullYear(),
+                    month: startTime.getMonth(),
+                    day: startTime.getDate(),
+                };
+                const endAt = {
+                    year: endTime.getFullYear(),
+                    month: endTime.getMonth(),
+                    day: endTime.getDate(),
+                };
+                if (
+                    startAt.year === endAt.year &&
+                    startAt.month === endAt.month &&
+                    startAt.day === endAt.day
+                ) {
+                    range = 'day';
+                } else if (
+                    startAt.year === endAt.year &&
+                    startAt.month === endAt.month
+                ) {
+                    range = 'month';
+                } else {
+                    range = 'year';
+                }
+            }
+        }
+    }
+    return range;
+};
 
 const Task = mongoose.model('Task', taskSchema);
 
--- src/routers/task.router.js
+++ src/routers/task.router.js
@@ -6,12 +6,18 @@ const router = new express.Router();
 
 //create a task
 router.post('/', auth, async (req, res) => {
-    const newTask = new Task({
-        ...req.body,
-        owner: req.user._id,
-    });
-
     try {
+        const range = Task.generateRangeProperty(req.body.date);
+
+        if (!range) {
+            return res
+                .status(400)
+                .send({ message: 'The end time must be after the start time' });
+        }
+
+        const data = { ...req.body, range, owner: req.user._id };
+        const newTask = new Task(data);
+
         await newTask.save();
         res.status(201).send(newTask);
     } catch (err) {
@@ -56,6 +62,9 @@ router.patch('/many', auth, async (req, res) => {
     const updateTasks = req.body;
     const allowedProperties = ['taskName', 'description', 'completed', 'date'];
     const isValid = updateTasks.every((obj) => {
+        if (obj.date && obj.date.startAt && obj.date.endAt) {
+            return obj.date.endAt > obj.date.startAt;
+        }
         return Object.keys(obj).every((property) => {
             return property === 'id' || allowedProperties.includes(property);
         });
@@ -65,27 +74,51 @@ router.patch('/many', auth, async (req, res) => {
     }
 
     let flag = true;
+    let idTaskNotFind;
+    const tasksFound = [];
 
     try {
-        //find task and update
+        //check idTask
         for (let update of updateTasks) {
             const task = await Task.findOne({
                 _id: update.id,
                 owner: req.user._id,
             });
             if (!task) {
+                idTaskNotFind = update.id;
                 flag = false;
                 break;
             }
-            Object.keys(update).forEach(
-                (property) => (task[property] = update[property]),
-            );
-
-            await task.save();
+            tasksFound.push(task);
         }
+
         if (!flag) {
-            res.status(404).send({ message: 'Cannot find tasks' });
+            res.status(404).send({
+                message: `Cannot find task has id: ${idTaskNotFind}`,
+            });
         } else {
+            //find and update task
+            for (let update of updateTasks) {
+                const task = tasksFound.find(
+                    (task) => update.id === task._id.toString(),
+                );
+                Object.keys(update).forEach((property) => {
+                    if (property === 'date') {
+                        const range = Task.generateRangeProperty(
+                            req.body[property],
+                        );
+                        if (range) {
+                            task.range = range;
+                        }
+
+                        task[property] = update[property];
+                    } else {
+                        task[property] = update[property];
+                    }
+                });
+
+                await task.save();
+            }
             res.send();
         }
     } catch (err) {
@@ -115,9 +148,22 @@ router.patch('/:id', auth, async (req, res) => {
             return res.status(404).send({ message: 'Cannot find task!' });
         }
 
-        updateProperties.forEach(
-            (property) => (task[property] = req.body[property]),
-        );
+        updateProperties.forEach((property) => {
+            if (property === 'date') {
+                const range = Task.generateRangeProperty(req.body[property]);
+
+                if (!range) {
+                    return res.status(400).send({
+                        message: 'The end time must be after the start time',
+                    });
+                }
+
+                task.range = range;
+                task.date = req.body.date;
+            } else {
+                task[property] = req.body[property];
+            }
+        });
 
         await task.save();
         res.send(task);
@@ -130,18 +176,34 @@ router.patch('/:id', auth, async (req, res) => {
 router.delete('/many', auth, async (req, res) => {
     try {
         let flag = true;
-        //find task and remove
+        let idTaskNotFind;
+        const tasksFound = [];
+        //check isTask
         for (let id of req.body) {
             const task = await Task.findOne({ _id: id, owner: req.user._id });
             if (!task) {
+                idTaskNotFind = id;
                 flag = false;
                 break;
             }
-            await task.remove();
+            tasksFound.push(task);
         }
         if (!flag) {
-            res.status(404).send({ message: 'Cannot find task' });
+            res.status(404).send({
+                message: `Cannot find task has id: ${idTaskNotFind}`,
+            });
         } else {
+            //find task and remove
+            for (let id of req.body) {
+                const task = tasksFound.find(
+                    (task) => task._id.toString() === id,
+                );
+                if (!task) {
+                    flag = false;
+                    break;
+                }
+                await task.remove();
+            }
             res.send();
         }
     } catch (err) {
